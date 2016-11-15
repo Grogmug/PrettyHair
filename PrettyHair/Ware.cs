4713b4501faaab19a46421d38ea66f0a2c0efb1d
Søren <Sren@DESKTOP-3DFGV3A>
2016-11-15 09:17:26 +0100

    Add project files.

---
+++ PrettyHair/Ware.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyHair
+{
+    class Ware
+    {
+        public int ID;
+        public decimal Price;
+        public int amount;
+        public  
+    }
+}
